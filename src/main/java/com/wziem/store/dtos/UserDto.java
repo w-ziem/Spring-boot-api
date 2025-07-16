64e5714fcf70c0e6b08f333b926e97ca6f9d26a6
w-ziem <ziembowiczwojciech@gmail.com>
2025-07-16 09:51:18 +0200

    @Json... annotations

--- src/main/java/com/wziem/store/dtos/UserDto.java
+++ src/main/java/com/wziem/store/dtos/UserDto.java
@@ -1,14 +1,25 @@
 package com.wziem.store.dtos;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDateTime;
 
 @AllArgsConstructor
 @Getter
 public class UserDto {
+    @JsonIgnore
     private Long id;
     private String name;
     private String email;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String phoneNumber;
+
+//    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+//    private LocalDateTime createdAt;
 
 }
--- src/main/java/com/wziem/store/mappers/UserMapper.java
+++ src/main/java/com/wziem/store/mappers/UserMapper.java
@@ -4,8 +4,10 @@ package com.wziem.store.mappers;
 import com.wziem.store.dtos.UserDto;
 import com.wziem.store.entities.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
+//    @Mapping(target = "createdAt", expression = "java(java.time.LocalDateTime.now())")
     UserDto toDto(User user);
 }
